b794faf7d36ce6f363894e828aafc8b6bb05085f
James Gardner <jamesgardner@Jamess-MacBook-Air.local>
2015-04-14 13:21:56 +0100

    formatting/styles

--- src/view/base.view.js
+++ src/view/base.view.js
@@ -1,45 +1,45 @@
 var BaseView = Backbone.View.extend({
-	constructor : function (options) {
-		_.extend(this, _.pick(options, 'template'));
+  constructor : function (options) {
+    _.extend(this, _.pick(options, 'template'));
 
-		this.views = new ViewContainer();
+    this.views = new ViewContainer();
 
-		if(options && options.views) {
-			_.each(options.views, this.addView, this);
-		}
+    if(options && options.views) {
+      _.each(options.views, this.addView, this);
+    }
 
-		this.options = options;
+    this.options = options;
 
-		Backbone.View.call(this, this.options);
-	},
+    Backbone.View.call(this, this.options);
+  },
 
-	addView : function (view) {
-		this.views.add(view);
-		this._addToBuffer(view);
-	},
+  addView : function (view) {
+    this.views.add(view);
+    this._addToBuffer(view);
+  },
 
-	_getBuffer : function () {
-		if(!this._buffer) {
-			this._buffer = document.createDocumentFragment();
-		}
+  _getBuffer : function () {
+    if(!this._buffer) {
+      this._buffer = document.createDocumentFragment();
+    }
 
-		return this._buffer;
-	},
+    return this._buffer;
+  },
 
-	_addToBuffer : function (view) {
-		this._getBuffer().appendChild(view.render().el);
-	},
+  _addToBuffer : function (view) {
+    this._getBuffer().appendChild(view.render().el);
+  },
 
-	getViewBy : function (criteria) {
-		return this.views.findWhere(criteria);
-	},
+  getViewBy : function (criteria) {
+    return this.views.findWhere(criteria);
+  },
 
-	render : function () {
-		var tmpl = $(this.template());
+  render : function () {
+    var tmpl = $(this.template());
 
-		this.$el.html(tmpl);
-		this.$el.append(this._buffer);
+    this.$el.html(tmpl);
+    this.$el.append(this._buffer);
 
-		return this;
-	}
+    return this;
+  }
 });
