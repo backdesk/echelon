5d136e2af09050e024c9c2d775c6e510912f64f6
James Gardner <crungmungus@gmail.com>
2015-04-23 16:42:29 +0100

    corrected indentation

--- test/spec/root.spec.js
+++ test/spec/root.spec.js
@@ -1,68 +1,69 @@
-describe('Root Factory', function () {	
-	var BasicView;
+describe('Root Factory', function() {
+  var BasicView;
 
-	beforeEach(function () {
-		BasicView = Backbone.View.extend({
-			render : function () {
-				return this;
-			}
-		});
+  beforeEach(function() {
+    BasicView = Backbone.View.extend({
+      render: function() {
+        return this;
+      }
+    });
 
-		this.root = Ech.root('', {
-			template : _.template('<div id="wrapper"></div>'),
+    this.root = Ech.root('', {
+      template: _.template('<div id="wrapper"></div>'),
 
-			id : 'test',
+      id: 'test',
 
-			className : 'testing',
+      className: 'testing',
 
-			views : [
-				new BasicView({
-					model : new Backbone.Model()
-				}),
-				new BasicView({
-					model : new Backbone.Model()
-				}),
-				new BasicView({
-					model : new Backbone.Model()
-				})
-			]
-		});
+      views: [
+        new BasicView({
+          model: new Backbone.Model()
+        }),
+        new BasicView({
+          model: new Backbone.Model()
+        }),
+        new BasicView({
+          model: new Backbone.Model()
+        })
+      ]
+    });
 
-		this.root.init();
-	});
+    this.root.init();
+  });
 
-	it('should return a new instance', function () {
-		var one = Ech.root(), two = Ech.root();
+  it('should return a new instance', function() {
+    var one = Ech.root(),
+      two = Ech.root();
 
-		expect(one).not.toBe(two);
-	});
+    expect(one).not.toBe(two);
+  });
 
-	it('should build given child views upon creation', function () {
-		expect($(this.root.getBuffer()).children().length).toBe(3);
-	});
+  it('should build given child views upon creation', function() {
+    expect($(this.root.getBuffer()).children().length).toBe(3);
+  });
 
-	it('should accept a template attribute', function () {
-		expect(this.root.template).toBeDefined();
-	});
+  it('should accept a template attribute', function() {
+    expect(this.root.template).toBeDefined();
+  });
 
-	it('should add given child views to array when instantiated', function () {
-		expect(this.root.views.length).toBe(3);
-	});
+  it('should add given child views to array when instantiated', function() {
+    expect(this.root.views.length).toBe(3);
+  });
 
-	it('should accept child views when addChildView is called', function () {
-		this.root.addChildView(new BasicView());
-		
-		expect($(this.root.getBuffer()).children().length).toBe(4);
-		expect(this.root.views.length).toBe(4);
-	});			
+  it('should accept child views when addChildView is called', function() {
+    this.root.addChildView(new BasicView());
 
-	it('should allow view to be retrieved by cid', function () {
-		var result, cid = this.root.last().cid;
+    expect($(this.root.getBuffer()).children().length).toBe(4);
+    expect(this.root.views.length).toBe(4);
+  });
 
-		result = this.root.findWhere({
-			cid : cid
-		});
+  it('should allow view to be retrieved by cid', function() {
+    var result, cid = this.root.last().cid;
 
-		expect(result.cid).toBe(cid);
-	});	
+    result = this.root.findWhere({
+      cid: cid
+    });
+
+    expect(result.cid).toBe(cid);
+  });
 });
